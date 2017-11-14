c64523a84b19dbe1cca86850bc5d4ff874fe4163
Pablo Avila <jose.9526@gmail.com>
2017-11-14 17:41:15 -0600

    Add timer to reduce the amount of instructions per second

--- Puppeteer/LEAP/LeapService.cpp
+++ Puppeteer/LEAP/LeapService.cpp
@@ -1,6 +1,13 @@
+
+// Standard Includes
 #include <iostream>
 #include <cstring>
+#include <ctime>
+
+// LEAP Includes
 #include "./Include/Leap.h"
+
+// Puppeteer Includes
 #include "../CommLib/Sockets.h"
 #include "../CommLib/Sockets.cpp"
 
@@ -13,6 +20,10 @@ class SampleListener : public Listener {
     Sockets Tx;
     string IP;
     int Port;
+    char mensaje[256];
+    clock_t t1;
+	  clock_t t2;
+  	double frec;
     virtual void onInit(const Controller&);
     virtual void onConnect(const Controller&);
     virtual void onFrame(const Controller&);
@@ -25,6 +36,14 @@ void SampleListener::onInit(const Controller& controller) {
 
   this->Tx.openCom(Port);
   this->Tx.initClient(c_IP, Port);
+  
+  for(int i =0; i<256; i++){
+		this->mensaje[i] =0;
+	}
+  
+  this->t1 = clock();
+  this->t2 = clock();
+  this->frec= 0.01;
 }
 
 void SampleListener::onConnect(const Controller& controller) {
@@ -36,89 +55,102 @@ void SampleListener::onConnect(const Controller& controller) {
 }
 
 void SampleListener::onFrame(const Controller& controller) {
-//    Controller controller;
+	this->t2 = clock();
+  clock_t milliseconds = this->t2 - this->t1;
+  float timediff_sec = ((float)milliseconds) / CLOCKS_PER_SEC;
+
   const Frame frame = controller.frame();	
   HandList hands = frame.hands();
 	Hand hand = hands[0];
 
-  char mensaje[256]; 
-	for(int i =0; i<256; i++){
-		mensaje[i] =0;
-	}
-	
+	// Get gestures
+	Leap::GestureList gestures = frame.gestures();
 
-  // Get gestures
-  Leap::GestureList gestures = frame.gestures();
-  for (int g = 0; g < gestures.count(); ++g) {
-    Gesture gesture = gestures[g];
-    switch (gesture.type()) {
+	for (int g = 0; g < gestures.count(); ++g) {
+	  Gesture gesture = gestures[g];
+	  
+	  switch (gesture.type()) {
+	    case Leap::Gesture::TYPE_CIRCLE:{
+				//Handle circle gestures
 
-      case Leap::Gesture::TYPE_CIRCLE:{
-      	//Handle circle gestures
-      	if(hand.isLeft()==false){
-		      mensaje[0] = '1';  // RISE/DOWN ARMS
-		      mensaje[1] = '6';
-		    }
-		    else{
-			    mensaje[0] = '7';  // OPEN RIGHT HAND
-		      mensaje[1] = 0;
-		    }
-        cout<<"RIGHT Circle: " <<mensaje << endl;
-        this->Tx.sendMessage(mensaje);
-        break;
-				}
-				
-      case Leap::Gesture::TYPE_KEY_TAP:{
-        //Handle key tap gestures
-        if(hand.isLeft()==false){
-		      mensaje[0] = '3';  // STAND UP
-		      mensaje[1] = 0;
-		    }
-		    else{
-			    mensaje[0] = '4';  // CROUCH
-		      mensaje[1] = 0;
-		    }
-        cout<<"RIGHT Key Tap: "<< mensaje << endl;
-        this->Tx.sendMessage(mensaje);
-        break;
-        }
-
-      case Leap::Gesture::TYPE_SWIPE:{
-        //Handle swipe gestures
-        if(hand.isLeft()==false){
-		      mensaje[0] = '8';  // WALK FORWARD
-		      mensaje[1] = 0;
-		    }
-		    else{
-			    mensaje[0] = '1';  // STOP MOVE
-		      mensaje[1] = '1';
-		    }
-        cout<< "RIGHT Swipe: "<<mensaje << endl;
-        this->Tx.sendMessage(mensaje);
-        break;
-        }
+	    	if(timediff_sec >= frec){
+					if(hand.isLeft()==false){
+						this->mensaje[0] = '1';  // RISE/DOWN ARMS
+						this->mensaje[1] = '6';
+					}
+					else{
+						this->mensaje[0] = '7';  // OPEN RIGHT HAND
+						this->mensaje[1] = 0;
+					}
+				  cout<<"RIGHT Circle: " <<this->mensaje << endl;
+				  this->Tx.sendMessage(this->mensaje);
+				  this->t1 = clock(); // Reset time count
+	      }
+	      break;
+			}
+			
+	    case Leap::Gesture::TYPE_KEY_TAP:{
+			  //Handle key tap gestures
+	    	
+	    	if(timediff_sec >= frec){
+			    if(hand.isLeft()==false){
+					  this->mensaje[0] = '3';  // STAND UP
+					  this->mensaje[1] = 0;
+					}
+					else{
+						this->mensaje[0] = '4';  // CROUCH
+					  this->mensaje[1] = 0;
+					}
+			    cout<<"RIGHT Key Tap: "<< this->mensaje << endl;
+			    this->Tx.sendMessage(this->mensaje);
+			    this->t1 = clock(); // Reset time count
+	      break;
+	      }
+			}
+	    
+	    case Leap::Gesture::TYPE_SWIPE:{
+	      //Handle swipe gestures
+	      if(timediff_sec >= frec){
+			    if(hand.isLeft()==false){
+					  this->mensaje[0] = '8';  // WALK FORWARD
+					  this->mensaje[1] = 0;
+					}
+					else{
+						this->mensaje[0] = '1';  // STOP MOVE
+					  this->mensaje[1] = '1';
+					}
+			    cout<< "RIGHT Swipe: "<<this->mensaje << endl;
+			    this->Tx.sendMessage(this->mensaje);
+			    this->t1 = clock(); // Reset time count
+	      }
+	      break;
+	    }
 
 			case Leap::Gesture::TYPE_SCREEN_TAP:{
-        //Handle screen tap gestures
-        if(hand.isLeft()==false){
-		      mensaje[0] = '1';  // CAMERA SELECTION
-		      mensaje[1] = '3';
-		    }
-		    else{
-			    mensaje[0] = '1';  // PICTURE CAPTURE
-		      mensaje[1] = '4';
-		    }
-        cout<< "RIGHT Tap Screen:"<< mensaje<< endl; 
-        //<< endl <<" (End communication sending )"<< endl;
-        this->Tx.sendMessage(mensaje);
-        //Tx.closeCom(this->Port);
-        break;
-        }
-        
-      default:
-        //Handle unrecognized gestures
-        break;
-    }
+			if(timediff_sec >= frec){
+			    //Handle screen tap gestures
+			    if(hand.isLeft()==false){
+					  this->mensaje[0] = '1';  // CAMERA SELECTION
+					  this->mensaje[1] = '3';
+					}
+					else{
+						this->mensaje[0] = '1';  // PICTURE CAPTURE
+					  this->mensaje[1] = '4';
+					}
+			    cout<< "RIGHT Tap Screen:"<< this->mensaje<< endl; 
+			    //<< endl <<" (End communication sending )"<< endl;
+			    this->Tx.sendMessage(this->mensaje);
+			    //Tx.closeCom(this->Port);
+			    this->t1 = clock(); // Reset time count
+				}
+	      break;
+	    }
+	      
+	    default:
+	      //Handle unrecognized gestures
+	      //this->t1 = clock(); // Reset time count
+	      break;  
+		}
   }
 }
 
@@ -138,10 +170,12 @@ int main(int argc, char *argv[]) {
   controller.addListener(listener);
 
 		//**************  INITIALIZE SYSTEM ****************
- 	  char init[]={'0',0}; 							// SPEAK
- 	  listener.Tx.sendMessage(init);
- 	  char cameraInit[] = {'1','2',0};	// CAMERA SETUP  
-		listener.Tx.sendMessage(cameraInit);
+		listener.mensaje[0] = '0';  // SPEAK
+	  listener.mensaje[1] = 0;					
+ 	  listener.Tx.sendMessage(listener.mensaje);
+ 	  listener.mensaje[0] = '1';  // CAMERA SETUP  
+	  listener.mensaje[1] = '2';
+		listener.Tx.sendMessage(listener.mensaje);
 
   // Keep this process running until Enter is pressed
   std::cout << "Press Enter to quit..." << std::endl;
