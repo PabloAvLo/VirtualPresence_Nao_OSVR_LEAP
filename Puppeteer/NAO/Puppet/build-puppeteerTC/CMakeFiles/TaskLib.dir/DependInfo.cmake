9b062630c8dfb54baa8064e1486dc3a5ecdc83c4
Pablo Avila <jose.9526@gmail.com>
2017-11-14 17:00:56 -0600

    LEAP working with default gestures with each hand to have 8 different instructions

---
+++ Puppeteer/NAO/Puppet/build-puppeteerTC/CMakeFiles/TaskLib.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pabloav/Desktop/Puppeteer/Nao/Server/TaskLib/TaskLib.cpp" "/home/pabloav/Desktop/Puppeteer/Nao/Server/TaskLib/build-puppeteerTC/CMakeFiles/TaskLib.dir/TaskLib.cpp.o"
+  "/home/pabloav/Desktop/Puppeteer/CommLib/Sockets.cpp" "/home/pabloav/Desktop/Puppeteer/Nao/Server/TaskLib/build-puppeteerTC/CMakeFiles/TaskLib.dir/home/pabloav/Desktop/Puppeteer/CommLib/Sockets.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/pabloav/Desktop/Puppeteer/Nao/ctc-linux64-atom-2.1.4.13/libnaoqi/include"
+  "/home/pabloav/Desktop/Puppeteer/Nao/ctc-linux64-atom-2.1.4.13/boost/include/boost-1_55"
+  "/home/pabloav/Desktop/Puppeteer/Nao/ctc-linux64-atom-2.1.4.13/cross/i686-aldebaran-linux-gnu/sysroot/usr/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Puppeteer/NAO/Puppet/build-puppeteerTC/CMakeFiles/TaskLib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TaskLib.dir/TaskLib.cpp.o"
+  "CMakeFiles/TaskLib.dir/home/pabloav/Desktop/Puppeteer/CommLib/Sockets.cpp.o"
+  "sdk/bin/TaskLib.pdb"
+  "sdk/bin/TaskLib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TaskLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
